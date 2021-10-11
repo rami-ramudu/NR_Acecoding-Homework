051ce29cd69e036a0ffb4c4f1d95f59485d0dde4
rami-ramudu <89202765+rami-ramudu@users.noreply.github.com>
2021-10-11 22:23:47 +0530

    Create Datatypes.cpp

---
+++ Day2/Datatypes.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float f;
+    double d;
+    string s;
+    long long l;
+
+    cout<<"Enter the value of a"<<"\n";
+    cin>>a;
+
+    cout<<"Enter the value of b"<<"\n";
+    cin>>b;
+
+    cout<<"Enter the value of c"<<"\n";
+    cin>>c;
+
+    cout<<"Enter the value of f"<<"\n";
+    cin>>f;
+
+    cout<<"Enter the value of d"<<"\n";
+    cin>>d;
+
+    cout<<"Enter the value of s"<<"\n";
+    cin>>s;
+
+    cout<<"Enter the value of l"<<"\n";
+    cin>>l;
+
+
+    cout<<a<<" "<<b<<" "<<c<<" "<<f<<" "<<d<<" "<<s<<" "<<l<<"\n";
+    return 0;
+}
