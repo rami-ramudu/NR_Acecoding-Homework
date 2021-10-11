a57d4cf9f3adb7b77a4e07623b4818d37c22ed46
rami-ramudu <89202765+rami-ramudu@users.noreply.github.com>
2021-10-11 23:00:04 +0530

    Create Pattern1.cpp

---
+++ Day4/Pattern1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=n-i;k++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<"*";
+        cout<<"\n";
+    }
+    return 0;
+}
+
+// 7
+//       *
+//      **
+//     ***
+//    ****
+//   *****
+//  ******
+// *******
